9e431dd1a566306f77d09eb06c3f6ca619cccd84
baahjeremiah754 <2425403300@live.gctu.edu.gh>
2026-05-31 20:09:19 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: To print out my initials
+# Assignment Number:Assignment Number 1
+#
+# Name: Jeremiah Baah
+# STUDENT ID:2425403300
+# Email:2425403300@live.gctu.edu.gh
+# Grader:Augustus Buckman
+#
+# On my honor,Jeremiah Baah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
 
+
+    print()
+    print("...JBJ")
+    print()
+    print("...JJJJJJJJJJJJ........BBBBBBBBBBB.........JJJJJJJJJJJJ.....")
+    print("...JJJJJJJJJJJJ........BBBB.....BBB........JJJJJJJJJJJJ.....")
+    print(".........JJJJJ.........BBBB.....BBB...............JJJJJ.....")
+    print(".........JJJJJ.........BBBBBBBBBBB................JJJJJ.....")
+    print(".........JJJJJ.........BBBBBBBB...................JJJJJ.....")
+    print(".........JJJJJ.........BBBBBBBBBBB................JJJJJ.....")
+    print("...JJJ...JJJJJ.........BBB......BBB........JJJ....JJJJJ.....")
+    print("...JJJ...JJJJJ.........BBB......BBB........JJJ...JJJJJJ.....")
+    print("...JJJJJJJJJJJJ...**...BBB......BBB...**...JJJJJJJJJJJJ...**")
+    print("...JJJJJJJJJJJJ...**...BBBBBBBBBBB....**...JJJJJJJJJJJJ...**")
+    print()
 
 
 main()
